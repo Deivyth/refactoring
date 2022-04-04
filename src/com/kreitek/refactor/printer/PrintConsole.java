522e2bff1162c0b5d43ddd3f8ccc7199b5f16af4
Deivyth Mena <driviti@hotmail.es>
2022-04-04 11:36:24 +0200

    Create class PrintConsole

---
+++ src/com/kreitek/refactor/printer/PrintConsole.java
@@ -0,0 +1,17 @@
+package com.kreitek.refactor.printer;
+
+import com.kreitek.refactor.TIPODNI;
+
+public class PrintConsole {
+
+    public static void header(){
+        System.out.println("=====================");
+        System.out.println("Vamos a refactorizar!");
+        System.out.println("=====================");
+    }
+
+    public static void result(TIPODNI tipo, String num, Boolean result){
+        System.out.println(tipo+" " + num + " es: " + result);
+    }
+
+}
